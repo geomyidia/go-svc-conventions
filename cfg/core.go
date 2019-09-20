891d7e5602fb198d3880fabe6e4818c413a5d98e
Duncan McGreggor <oubiwann@gmail.com>
2019-09-20 08:51:46 -0500

    Added docstring about in-memory config.

--- cfg/core.go
+++ cfg/core.go
@@ -1,10 +1,10 @@
 package cfg
 
 import (
-	"log"
 	"strings"
 
 	cfg "github.com/spf13/viper"
+	log "github.com/sirupsen/logrus"
 	logger "github.com/geomyidia/zylog/logger"
 )
 
@@ -60,8 +60,19 @@ type Config struct {
 	Logging *logger.ZyLogOptions
 }
 
-// NewConfig ...
-// XXX Fod: does Viper provide something like this that we could use?
+// NewConfig is a constructor that creates the full coniguration data structure 
+// for use by our application(s) and client(s) as an in-memory copy of the 
+// config data (saving from having to make repeated and somewhat expensive 
+// calls to the viper library). 
+// 
+// Note that Viper does provide both the AllSettings() and Unmarshall()
+// functions, but these require that you have a struct defined that will be 
+// used to dump the Viper config data into. We've already got that set up, so
+// there's no real benefit to switching.
+//
+// Furthermore, in our case, we're utilizing structs from other libraries to
+// be used when setting those up (see how we initialize the logging component
+// in ./components/logging.go, Setup).
 func NewConfig() *Config {
 	return &Config{
 		HTTPD: HTTPDConfig{
